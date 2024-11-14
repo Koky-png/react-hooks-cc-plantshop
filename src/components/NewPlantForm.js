bc9906c712d21696ae94f776987d72ff75d419db
Njoki <kokymash@gmail.com>
2024-11-14 12:41:00 +0300

    allfiles

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -17,7 +17,7 @@ function NewPlantForm({ setPlants }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("http://localhost:5000/plants", {
+    fetch("https://react-hooks-cc-plantshop-rksc.onrender.com/plants", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -8,7 +8,7 @@ function PlantPage() {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:5000/plants")
+    fetch("https://react-hooks-cc-plantshop-rksc.onrender.com/plants")
       .then((response) => response.json())
       .then((data) => setPlants(data));
   }, []);
